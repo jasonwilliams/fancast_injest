2ccd8841b4903923cf1167238675544c7ec925d9
Jason Williams <jason.williams01@bbc.co.uk>
2018-09-18 17:51:47 +0100

    adding api

---
+++ api/api.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// API Entrypoint to the API
+func API() {
+	router := mux.NewRouter()
+	router.HandleFunc("/test", Test).Methods("GET")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", router))
+}
+
+// Test is a testing function
+func Test(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "", "hello world")
+}
