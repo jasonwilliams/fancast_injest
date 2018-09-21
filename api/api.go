2931ac73f2c1cca6f402a96dd57c7a2439ed3138
Jason Williams <jason.williams01@bbc.co.uk>
2018-09-21 17:41:56 +0100

    adding routes

--- api/api.go
+++ api/api.go
@@ -12,10 +12,22 @@ import (
 func API() {
 	router := mux.NewRouter()
 	router.HandleFunc("/test", Test).Methods("GET")
+	router.HandleFunc("/podcasts/{podcast}", podcastHandler)
+	router.HandleFunc("/podcasts/{podcast}/episodes", podcastEpisodesHandler)
 	log.Fatal(http.ListenAndServe("0.0.0.0:8060", router))
 }
 
 // Test is a testing function
 func Test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "", "hello world")
+}
+
+// Handle the podcast homepage
+func podcastHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Handle fetching episodes for a podcast
+func podcastEpisodesHandler(w http.ResponseWriter, r *http.Request) {
+
 }
