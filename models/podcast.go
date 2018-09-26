dd62579ae62ef3767a69f4bd5f7647a0148076b1
Jason Williams <jase.williams@gmail.com>
2018-09-26 22:46:39 +0100

    updates

--- models/podcast.go
+++ models/podcast.go
@@ -2,15 +2,18 @@ package models
 
 import (
 	"encoding/json"
-	"fmt"
+	"time"
+
+	"bitbucket.org/jayflux/mypodcasts_injest/logger"
 )
 
 // Podcast represents the structure of a podcast
 type Podcast struct {
-	ID          string          `db:"id" json:"id"`
-	Title       string          `db:"title" json:"title"`
-	Description string          `db:"description" json:"description"`
-	Image       json.RawMessage `db:"image" json:"image"`
+	ID          string           `db:"id" json:"id"`
+	Title       string           `db:"title" json:"title"`
+	Description string           `db:"description" json:"description"`
+	Image       json.RawMessage  `db:"image" json:"image"`
+	Episodes    []PodcastEpisode `db:"episodes" json:"episodes"`
 }
 
 // GetPodcast returns a Podcast struct
@@ -19,8 +22,10 @@ func GetPodcast(id string) Podcast {
 	row := db.QueryRow("SELECT id, title, description, image FROM podcasts where id = $1", id)
 	err := row.Scan(&podcast.ID, &podcast.Title, &podcast.Description, &podcast.Image)
 	if err != nil {
-		fmt.Println(err)
+		logger.Log.Println(err)
 	}
+
+	podcast.Episodes = podcast.GetEpisodes()
 	return podcast
 }
 
@@ -29,4 +34,17 @@ func (p Podcast) GetImage() (PodcastImage, error) {
 	var podcastImage PodcastImage
 	error := json.Unmarshal(p.Image, podcastImage)
 	return podcastImage, error
+}
+
+// GetEpisodes fetches the first 20 episodes related to this podcast
+func (p Podcast) GetEpisodes() []PodcastEpisode {
+	// First lets get a date from the past
+	datetime, err := time.Parse(time.RFC3339, "1990-08-24T11:00:00Z")
+	if err != nil {
+		logger.Log.Println(err)
+	}
+
+	podcastEpisodes := GetPodcastEpisodes(p.ID, datetime)
+	return podcastEpisodes
+
 }
--- models/podcastEpisode.go
+++ models/podcastEpisode.go
@@ -2,22 +2,46 @@ package models
 
 import (
 	"encoding/json"
+	"time"
+
+	"bitbucket.org/jayflux/mypodcasts_injest/logger"
 )
 
 // PodcastEpisode represents the structure of a podcast
 type PodcastEpisode struct {
-	ID          string          `db:"id" json:"id"`
-	Title       string          `db:"title" json:"title"`
-	Description string          `db:"description" json:"description"`
-	Image       json.RawMessage `db:"image" json:"image"`
+	ID              string          `db:"id" json:"id"`
+	Title           string          `db:"title" json:"title"`
+	Description     string          `db:"description" json:"description"`
+	Image           json.RawMessage `db:"image" json:"image"`
+	PublishedParsed string          `db:"published_parsed" json:"publishedParsed"`
 }
 
 // GetPodcastEpisode returns a Podcast struct
 func GetPodcastEpisode(id string) PodcastEpisode {
 	var podcastEpisode PodcastEpisode
-	row := db.QueryRow("SELECT id, title, description, image FROM podcast_episodes where id = $1", id)
-	row.Scan(&podcastEpisode.ID, &podcastEpisode.Title, &podcastEpisode.Description, &podcastEpisode.Image)
+	row := db.QueryRow("SELECT id, title, description, image, published_parsed FROM podcast_episodes where id = $1", id)
+	row.Scan(&podcastEpisode.ID, &podcastEpisode.Title, &podcastEpisode.Description, &podcastEpisode.Image, &podcastEpisode.PublishedParsed)
 	return podcastEpisode
+}
+
+// GetPodcastEpisodes returns multiple episodes based on a datetime
+// Example datetime from database - 2018-08-24T11:00:00Z
+func GetPodcastEpisodes(id string, datetime time.Time) []PodcastEpisode {
+	var podcastEpisodes []PodcastEpisode
+	rows, err := db.Query("SELECT id, title, description, image, published_parsed FROM podcast_episodes where parent = $1 AND published_parsed > $2", id, datetime)
+	if err != nil {
+		logger.Log.Println(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var podcastEpisode PodcastEpisode
+		if err := rows.Scan(&podcastEpisode.ID, &podcastEpisode.Title, &podcastEpisode.Description, &podcastEpisode.Image, &podcastEpisode.PublishedParsed); err != nil {
+			logger.Log.Fatal(err)
+		}
+		podcastEpisodes = append(podcastEpisodes, podcastEpisode)
+	}
+
+	return podcastEpisodes
 }
 
 // GetImage gets the podcast image for this current Podcast struct
